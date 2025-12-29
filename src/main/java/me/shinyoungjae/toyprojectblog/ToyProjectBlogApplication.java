e2af9a2a89c142404bf44563f4696b59c60643b7
Shin-youngjae <a01037497436@gmail.com>
2025-12-29 23:43:16 +0900

    feat: 블로그를 실행시키기 위한 main 클래스

---
+++ src/main/java/me/shinyoungjae/toyprojectblog/ToyProjectBlogApplication.java
@@ -0,0 +1,11 @@
+package me.shinyoungjae.toyprojectblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToyProjectBlogApplication.class, args);
+    }
+}
