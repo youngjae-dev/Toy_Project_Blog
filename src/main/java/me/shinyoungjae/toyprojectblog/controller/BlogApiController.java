32eb62119bd1ad09717dfbdd4090fcfa56d1928c
Shin-youngjae <a01037497436@gmail.com>
2026-01-05 22:27:19 +0900

    feat : GET 요청 시 글 목록을 조회할 함수

--- src/main/java/me/shinyoungjae/toyprojectblog/controller/BlogApiController.java
+++ src/main/java/me/shinyoungjae/toyprojectblog/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.shinyoungjae.toyprojectblog.controller;
 import lombok.RequiredArgsConstructor;
 import me.shinyoungjae.toyprojectblog.domain.Article;
 import me.shinyoungjae.toyprojectblog.dto.AddArticleRequest;
+import me.shinyoungjae.toyprojectblog.dto.ArticleResponse;
 import me.shinyoungjae.toyprojectblog.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -24,5 +28,16 @@ public class BlogApiController {
 
         // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    // /api/articles GET 요청이 들어오면 글 목록을 조회할 메서드
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(articles);
     }
 }
