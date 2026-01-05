bcffc6e99f2b2239baaecbce436810bc2050c976
Shin-youngjae <a01037497436@gmail.com>
2026-01-05 22:19:37 +0900

    feat : DB에 저장되어 있는 글 모두 가져오는 메서드 추가

--- src/main/java/me/shinyoungjae/toyprojectblog/service/BlogService.java
+++ src/main/java/me/shinyoungjae/toyprojectblog/service/BlogService.java
@@ -6,6 +6,8 @@ import me.shinyoungjae.toyprojectblog.dto.AddArticleRequest;
 import me.shinyoungjae.toyprojectblog.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service // 빈으로 등록
 public class BlogService {
@@ -15,5 +17,10 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // DB에 저장되어 있는 글 모두 가져오는 메서드
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
