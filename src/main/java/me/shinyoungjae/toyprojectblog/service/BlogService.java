04fb194badd8f1b378b072a99f4ca350f198f924
Shin-youngjae <a01037497436@gmail.com>
2026-01-09 15:38:18 +0900

    feat : 블로그 글 삭제 서비스 메서드 추가

--- src/main/java/me/shinyoungjae/toyprojectblog/service/BlogService.java
+++ src/main/java/me/shinyoungjae/toyprojectblog/service/BlogService.java
@@ -29,4 +29,9 @@ public class BlogService {
         return blogRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("nof found: " + id));
     }
+
+    // 블로그 글 삭제 메서드
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
 }
