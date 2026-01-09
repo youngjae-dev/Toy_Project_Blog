c52df4e654d5a5ab249050707a7a6a8b69ab7ef3
Shin-youngjae <a01037497436@gmail.com>
2026-01-09 15:21:33 +0900

    feat : 블로그 글 목록을 조회하는 메서드 추가

--- src/main/java/me/shinyoungjae/toyprojectblog/service/BlogService.java
+++ src/main/java/me/shinyoungjae/toyprojectblog/service/BlogService.java
@@ -23,4 +23,10 @@ public class BlogService {
     public List<Article> findAll() {
         return blogRepository.findAll();
     }
+
+    // 블로그 글 조회 메서드
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("nof found: " + id));
+    }
 }
