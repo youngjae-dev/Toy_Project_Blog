ad57ecbb5276f8a20addee33e735b3929e60c3b6
Shin-youngjae <a01037497436@gmail.com>
2026-01-28 22:22:30 +0900

    feat : 뷰에게 데이터를 전달하기 위한 객체 생성 용도

--- src/main/java/me/shinyoungjae/toyprojectblog/dto/ArticleListViewResponse.java
+++ src/main/java/me/shinyoungjae/toyprojectblog/dto/ArticleListViewResponse.java
@@ -1,4 +1,18 @@
 package me.shinyoungjae.toyprojectblog.dto;
 
+import lombok.Getter;
+import me.shinyoungjae.toyprojectblog.domain.Article;
+
+@Getter
 public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
 }
